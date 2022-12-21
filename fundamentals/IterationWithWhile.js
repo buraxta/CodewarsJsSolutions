0ee677f398033c1fecad71d5d07c21cffa55ceff
Burak Güllüler <burakgulluler@gmail.com>
2022-12-21 23:40:27 +0300

    some study

---
+++ fundamentals/IterationWithWhile.js
@@ -0,0 +1,8 @@
+const myArray = [];
+
+// Only change code below this line
+let x = 5;
+while(x>=0){
+  myArray.push(x--);
+}
+console.log(myArray);
---
+++ fundamentals/recursion.js
@@ -0,0 +1,15 @@
+/*
+Write a recursive function, sum(arr, n), that returns the sum of the first n elements of an array arr.
+*/
+
+function sum(arr, n) {
+    // Only change code below this line
+    if(n == 0){
+        return 0;
+    }
+    return sum(arr, n-1) + arr[n-1];
+    // Only change code above this line
+  }
+
+  console.log(sum([2, 3, 4], 1)); // = 2
+  
