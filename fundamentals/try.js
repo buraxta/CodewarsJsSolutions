5a8766b4ef67dd4afe2587a0fbf253acffcd6ef3
Burak Güllüler <burakgulluler@gmail.com>
2023-01-01 21:23:44 +0300

    study

--- fundamentals/try.js
+++ fundamentals/try.js
@@ -1,7 +1,8 @@
-//Write a regex and use the appropriate string methods to remove whitespace at the beginning and end of strings.
+function myFunction() {
+  return "You rock!";
+}
+let varOne = myFunction;
+let varTwo = myFunction();
 
-//Note: The String.prototype.trim() method would work here, but you'll need to complete this challenge using regular expressions.
-let hello = "   Hello, World!  ";
-let wsRegex = /change/; // Change this line
-let result = hello; // Change this line
-
+let x = varOne();
+console.log(x); // You rock!
