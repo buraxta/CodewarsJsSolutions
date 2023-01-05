77140f70907b6b20a451242c18cce1738a00c48f
Burak Güllüler <burakgulluler@gmail.com>
2023-01-05 23:36:15 +0300

    study

--- fundamentals/try.js
+++
@@ -1,5 +0,0 @@
-//We have defined a function, htmlColorNames, which takes an array of HTML colors as an argument. Modify the function using splice() to remove the first two elements of the array and add 'DarkSalmon' and 'BlanchedAlmond' in their respective places.
-function htmlColorNames(arr) {
-  // change code below this line
-  arr.splice(0,2,'DarkSalmon','BlanchedAlmond');
-  // change code above this line
